578757fba0fddcc61e770067d237be63ce3b005c
“dgroen83“ <david.groen@duo.nl>
2018-06-11 21:09:04 +0200

    login via de juiste service laten lopen geen check meer in de frontend

---
+++ frontend/src/js/login.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+    $('#check_user').submit(function (e) {
+        e.preventDefault();
+
+        var $results = $('#password-check-results'),
+            username = $('#txt-username').val(),
+            password = $('#txt-password').val();
+
+        var requestURL = 'http://localhost:8080/login?';
+
+
+        $.getJSON(requestURL,
+            {
+                'username': username,
+                'password': password
+            }
+            , function (data) {
+                console.log(data);
+                if (data) {
+                    localStorage.setItem('welcomeName', username.toString());
+                    window.open("../src/welkom.html", "_self")
+                } else {
+                    $results.html('<p>Sorry your password is incorrect</p>');
+                }
+            });
+
+    })
+})
